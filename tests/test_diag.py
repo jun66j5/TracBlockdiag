394d0cfdaa1fa3e86bd7f59f89c65c6cc1f5716d
Jun Omae <jun66j5@gmail.com>
2016-07-02 06:19:27 +0900

    fix a failure in test_diag.py

--- tests/test_diag.py
+++ tests/test_diag.py
@@ -17,7 +17,7 @@ def skipUnlessImport(module):
 
 class TestBlockdiagBuilder(unittest.TestCase):
     png_header = (0x89, 0x50, 0x4E, 0x47, 0x0D, 0x0A, 0x1A, 0x0A)
-    blockdiag_text = """
+    blockdiag_text = u"""
     {
       A -> B -> C;
            B -> D;
